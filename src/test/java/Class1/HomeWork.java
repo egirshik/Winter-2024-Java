fa7e997a66a328cceecc995fa582bd40824508ff
liz <liz@gmail.com>
2024-12-18 22:04:41 -0500

    first git push

---
+++ src/test/java/Class1/HomeWork.java
@@ -0,0 +1,16 @@
+package Class1;
+
+public class HomeWork {
+
+    public static void main(String[] args) {
+
+        //      C to F
+        double cTemp = 40;
+        double fTemp_cTemp = (cTemp * 5 / 9) + 32;
+        System.out.println(cTemp + "C is equal to " + fTemp_cTemp + "F");
+        //      C to K
+
+
+
+    }
+}
